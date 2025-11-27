7e358e4feacb50babccf8e8df17f0f367f47d8ed
ShubboXD <subhodipsikdar@gmail.com>
2025-11-27 23:18:45 +0530

    .

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ fn main () {
     let mut guess = String::new();
     print!("Enter your guess: ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut guess).unwrap();
-    let guess: i32 = guess.trim().parse().unwrap();
+    io::stdin().read_line(&mut guess)
+    .expect("Failed to read line");
+    let guess: i32 = guess.trim().parse()
+    .expect("Need a number Input");
     let x = 1;
     if guess == x {
         println!("You won")
