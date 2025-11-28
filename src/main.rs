7e83b241994200afb9714efbf8699d0d1559244c
ShubboXD <subhodipsikdar@gmail.com>
2025-11-28 14:56:39 +0530

    .

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+#![allow(unused)]
 use std::io::{self, Write};
+use rand::Rng;
 
 fn main () {
+    let mut rng = rand::thread_rng();
     let mut guess = String::new();
+    let x = rng.gen_range(0..=1);
     print!("Enter your guess: ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut guess)
     .expect("Failed to read line");
     let guess: i32 = guess.trim().parse()
     .expect("Need a number Input");
-    let x = 1;
+    
     if guess == x {
         println!("You won")
     } else {
